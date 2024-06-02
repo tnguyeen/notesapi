efa03c6ecc63b0071c123bf7cb6613d28c91bf0b
tnguyeen <tainguyen.pham.133@gmail.com>
2024-06-02 15:09:59 +0700

    hinh nhu xong roi

---
+++ models/noteModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+
+const noteSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  isPinned: { type: Boolean, default: false },
+  userId: { type: String, required: true },
+  createOn: { type: Date, required: true, default: new Date().getTime() },
+})
+
+export default mongoose.model("Note", noteSchema)
--- utils.js
+++ utils.js
@@ -1,12 +1,12 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken"
 export default function authenticateToken(req, res, next) {
-    const authHeader = req.headers["authorization"]
-    const token = authHeader && authHeader.split(" ")[1]
+  const authHeader = req.headers["authorization"]
+  const token = authHeader && authHeader.split(" ")[1]
 
-    if (!token) return res.sendStatus(401)
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
-        if (error) return res.sendStatus(401)
-        req.user = user
-        next()
-    })
+  if (!token) return res.sendStatus(401)
+  jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+    if (error) return res.sendStatus(401)
+    req.user = user
+    next()
+  })
 }
