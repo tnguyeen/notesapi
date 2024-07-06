b75200f6a209522e5ff6b20f2f710bd448846e0e
tnguyeen <tainguyen.pham.133@gmail.com>
2024-07-06 16:58:44 +0700

    sua required

--- models/noteModel.js
+++ models/noteModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const noteSchema = new Schema({
   title: { type: String, required: true },
-  content: { type: String, required: true },
+  content: { type: String, required: false },
   isPinned: { type: Boolean, default: false },
   userId: { type: String, required: true },
   createOn: { type: Date, required: true, default: new Date().getTime() },
